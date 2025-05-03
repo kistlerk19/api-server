1d0e0cdd5ba122bc6200152f0019763ed0c7ecb5
Ishmael Gyamfi <ishmael.gyamfi.dev@gmail.com>
2025-05-03 16:49:22 +0000

    fix route controller path

--- server_api/routes/api.php
+++ server_api/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\ReportController;
+use App\Http\Controllers\Api\ReportController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
